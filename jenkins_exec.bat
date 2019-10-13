fadc5606690079d2f373a8844b1b2a8dec31cb16
AVK Kiran Kumar <avk.kiran@sails.solutions>
2019-10-13 21:13:36 +0530

    updating the command
    updating the command.

--- jenkins_exec.bat
+++ jenkins_exec.bat
@@ -1,1 +1,2 @@
-javaw -jar ./hackathon-api/target/hackathon-0.0.1-SNAPSHOT.jar &
+REM javaw -jar ./hackathon-api/target/hackathon-0.0.1-SNAPSHOT.jar &
+start /b javaw -jar ./hackathon-api/target/hackathon-0.0.1-SNAPSHOT.jar &
