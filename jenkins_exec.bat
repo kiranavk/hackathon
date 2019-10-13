5e426ea5d08047cc74cdd277dd784aec047b4147
AVK Kiran Kumar <avk.kiran@sails.solutions>
2019-10-13 22:49:52 +0530

    updating commands
    updating commands

--- jenkins_exec.bat
+++ jenkins_exec.bat
@@ -3,3 +3,6 @@ netstat -ano | find "9090"
 taskkill /F /PID 18396
 FOR /F "tokens=4 delims= " %P IN ('netstat -a -n -o ^| findstr :9090') DO TaskKill.exe /PID %P
 start /b javaw -jar ./hackathon-api/target/hackathon-0.0.1-SNAPSHOT.jar &
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm
+sudo yum install -y jdk-8u141-linux-x64.rpm
