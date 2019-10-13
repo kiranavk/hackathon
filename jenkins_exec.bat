ff7b1965eca5c6c59fe3dcc200456022911cc7e8
AVK Kiran Kumar <avk.kiran@sails.solutions>
2019-10-13 21:33:52 +0530

    Updating commands
    updating commands

--- jenkins_exec.bat
+++ jenkins_exec.bat
@@ -1,2 +1,5 @@
 REM javaw -jar ./hackathon-api/target/hackathon-0.0.1-SNAPSHOT.jar &
+netstat -ano | find "9090"
+taskkill /F /PID 18396
+FOR /F "tokens=4 delims= " %P IN ('netstat -a -n -o ^| findstr :9090') DO TaskKill.exe /PID %P
 start /b javaw -jar ./hackathon-api/target/hackathon-0.0.1-SNAPSHOT.jar &
