5c7c62dfeec04a9b8d4dcc2265a8b1a1ba2faa7e
AVK Kiran Kumar <avk.kirankumar@gmail.com>
2019-10-14 16:00:02 +0530

    updating commands
    updating commands

--- jenkins_exec.bat
+++ jenkins_exec.bat
@@ -7,3 +7,5 @@ start /b javaw -jar ./hackathon-api/target/hackathon-0.0.1-SNAPSHOT.jar &
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm
 sudo yum install -y jdk-8u141-linux-x64.rpm
 nohup java -jar hackathon-0.0.1-SNAPSHOT.jar > demo-app.log 2>&1 &
+
+scp hackathon-0.0.1-SNAPSHOT.jar ninjaroot@52.66.211.71:~/
